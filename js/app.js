4368202c1423c04c47ba427933574ff4257a461b
unknown <bassel.ashraf.gharib@gmail.com>
2021-08-21 18:22:22 +0200

    added a navigator bar & added a 4th section in the html

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@
  * 
  */
 
+const listContainer = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
@@ -33,13 +34,46 @@
  * 
  */
 
+// back to top of the page on load
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+}
+
 // build the nav
 
+// add ID to all h2 tags
+const sectionTitles = document.querySelectorAll("h2");
+
+for (let sectionId = 0; sectionId < sectionTitles.length; sectionId++) {
+    let att = document.createAttribute("id")
+    att.value = sectionId + 1; // #1, #2, etc..
+    sectionTitles[sectionId].setAttributeNode(att); // add the attribute to given section title ex: <h2 id="1">
+}
+
+// get coordinates of sections
+let sectionCoordinates = [
+    document.getElementById("1").getBoundingClientRect(),
+    document.getElementById("2").getBoundingClientRect(),
+    document.getElementById("3").getBoundingClientRect(),
+    document.getElementById("4").getBoundingClientRect(),
+]
+
+for (let sectionNum = 1; sectionNum <= 4; sectionNum++) {
+
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `Section ${sectionNum}`;
+
+    // Scroll to anchor ID using scrollTO event
+    listItem.addEventListener('click', function() {
+        let sectionCoordinateY = sectionCoordinates[sectionNum - 1].y
+        window.scrollTo(0, sectionCoordinateY - 150)
+    })
+
+    listContainer.appendChild(listItem);
+}
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
