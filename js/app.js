bc9fefe6c2f415171a6dfa1bcc20d26f64bd9dec
unknown <bassel.ashraf.gharib@gmail.com>
2021-08-22 22:35:30 +0200

    added active state & refactored the code to be dynamic

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
  */
 
 const listContainer = document.getElementById("navbar__list");
-
+const sectionContainers = document.getElementsByTagName("section");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -51,22 +51,22 @@ for (let sectionId = 0; sectionId < sectionTitles.length; sectionId++) {
 }
 
 // get coordinates of sections
-let sectionCoordinates = [
-    document.getElementById("1").getBoundingClientRect(),
-    document.getElementById("2").getBoundingClientRect(),
-    document.getElementById("3").getBoundingClientRect(),
-    document.getElementById("4").getBoundingClientRect(),
-]
+let sectionCoordinates = [];
+for (let sectionTitleId = 1; sectionTitleId <= sectionContainers.length; sectionTitleId++) {
+    sectionCoordinates.push(
+        document.getElementById(sectionTitleId).getBoundingClientRect() //push section to the array
+    );
+}
 
-for (let sectionNum = 1; sectionNum <= 4; sectionNum++) {
-
+// Build menu 
+for (let sectionNum = 1; sectionNum <= sectionContainers.length; sectionNum++) {
     const listItem = document.createElement("li");
-
     listItem.innerHTML = `Section ${sectionNum}`;
 
-    // Scroll to anchor ID using scrollTO event
+    // Scroll to section on link click
     listItem.addEventListener('click', function() {
-        let sectionCoordinateY = sectionCoordinates[sectionNum - 1].y
+        let sectionCoordinateY = sectionCoordinates[sectionNum - 1].y;
+        // Scroll to anchor ID using scrollTO event
         window.scrollTo(0, sectionCoordinateY - 150)
     })
 
@@ -74,16 +74,39 @@ for (let sectionNum = 1; sectionNum <= 4; sectionNum++) {
 }
 
 // Add class 'active' to section when near top of viewport
+for (let sectionNum = 1; sectionNum < sectionContainers.length; sectionNum++) {
+    let classAttr = document.createAttribute("class")
+    sectionContainers[sectionNum].setAttributeNode(classAttr);
+}
 
+// add active state by scroll event (Y axis coordinates)
+document.addEventListener('scroll', function() {
+    let scrollPositionY = window.scrollY; // get Y axis coordinates
+    for (let sectionIndex = 0; sectionIndex < sectionContainers.length; sectionIndex++) {
+        if (sectionIndex === sectionContainers.length - 1) {
+            if (sectionCoordinates[sectionIndex].y && (scrollPositionY > (sectionCoordinates[sectionIndex].y - 150))) {
+                toggleActive(sectionIndex);
+            }
+        } else if ((scrollPositionY >= (sectionCoordinates[sectionIndex].y - 150)) && (scrollPositionY < (sectionCoordinates[sectionIndex + 1].y - 150))) {
+            toggleActive(sectionIndex);
+        }
+    }
+});
+
+// Set sections as active
+function toggleActive(addIndex) {
+    for (let sectionId = 0; sectionId < sectionContainers.length; sectionId++) {
+        if (addIndex === sectionId) {
+            sectionContainers[sectionId].classList.add("active") // add active state when selected section is in viewport
+            continue;
+        } else {
+            sectionContainers[sectionId].classList.remove("active") // remove active state when selected section is in viewport
+        }
+    }
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
  */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
